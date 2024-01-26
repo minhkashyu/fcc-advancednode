52244d181a5259445da930f5554afff03c033ca9
Minh Ta <minhvinhta@yahoo.com>
2024-01-26 23:11:16 +1000

    20. Authentication with Socket.IO

--- server.js
+++ server.js
@@ -13,18 +13,48 @@ const routes = require('./routes.js');
 const auth = require('./auth');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
+const passportSocketIo = require('passport.socketio');
+const cookieParser = require('cookie-parser');
+
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
+  key: 'express.sid',
   secret: process.env.SESSION_SECRET,
   resave: true,
   saveUninitialized: true,
+  store: store,
   cookie: { secure: false }
 }));
 app.use(passport.initialize({}));
 app.use(passport.session({}));
+
+function onAuthorizeSuccess(data, accept) {
+  console.log('successful connection to socket.io');
+  accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+  if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
+
+io.use(
+    passportSocketIo.authorize({
+      cookieParser: cookieParser,
+      key: 'express.sid',
+      secret: process.env.SESSION_SECRET,
+      store: store,
+      success: onAuthorizeSuccess,
+      fail: onAuthorizeFail
+    })
+);
 
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
@@ -48,6 +78,7 @@ http.listen(PORT, () => {
 let currentUsers = 0;
 io.on('connection', socket => {
   console.log('A user has connected');
+  console.log('user ' + socket.request.user.username + ' connected');
 
   ++currentUsers;
 
