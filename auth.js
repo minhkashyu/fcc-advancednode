ddfb329f6b1dfdaf48910add0d18c873853d5575
Minh Ta <minhvinhta@yahoo.com>
2024-01-26 22:23:38 +1000

    16. Implementation of Social Authentication III

--- auth.js
+++ auth.js
@@ -1,15 +1,32 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
-const GithubStrategy = require('passport-github');
+const GitHubStrategy = require('passport-github').Strategy;
 const bcrypt = require('bcrypt');
 const {ObjectId} = require('mongodb');
 
 module.exports = function (app, myDataBase) {
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({_id: new ObjectId(id)}, (err, doc) => {
+      if (err) {
+        return done(err);
+      }
+      done(null, doc);
+    });
+  });
+
   passport.use(new LocalStrategy((username, password, done) => {
     myDataBase.findOne({username: username}, (err, user) => {
       console.log(`User ${username} attempted to log in.`);
-      if (err) return done(err);
-      if (!user) return done(null, false);
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false);
+      }
       if (!bcrypt.compareSync(password, user.password)) {
         return done(null, false);
       }
@@ -17,22 +34,37 @@ module.exports = function (app, myDataBase) {
     });
   }));
 
-  passport.use(new GithubStrategy({
+  passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: '/auth/github/callback'
-  }, (accessToken, refreshToken, profile, done) => {
+    callbackURL: process.env.GITHUB_CALLBACK_URL
+  }, (accessToken, refreshToken, profile, cb) => {
     console.log(profile);
-    //Database logic here with callback containing your user object
+    myDataBase.findOneAndUpdate(
+      { id: profile.id },
+      {
+        $setOnInsert: {
+          id: profile.id,
+          username: profile.username,
+          name: profile.displayName || 'John Doe',
+          photo: profile.photos[0].value || '',
+          email: Array.isArray(profile.emails)
+              ? profile.emails[0].value
+              : 'No public email',
+          created_on: new Date(),
+          provider: profile.provider || ''
+        },
+        $set: {
+          last_login: new Date()
+        },
+        $inc: {
+          login_count: 1
+        }
+      },
+      { upsert: true, new: true },
+      (err, doc) => {
+        return cb(null, doc.value);
+      }
+    );
   }));
-
-  passport.serializeUser((user, done) => {
-    done(null, user._id);
-  });
-
-  passport.deserializeUser((id, done) => {
-    myDataBase.findOne({_id: new ObjectId(id)}, (err, doc) => {
-      done(null, doc);
-    });
-  });
 };
